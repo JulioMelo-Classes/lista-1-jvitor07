d14de9361e2bdfe3e0b8c666ea1df9607fc34cf6
João Oliveira <jvitor2000@outlook.com>
2021-06-09 07:16:58 -0300

    resposta negativos5

--- autor.md
+++ autor.md
@@ -4,7 +4,7 @@ Preencha os dados abaixo para identificar a autoria do trabalho.
 
 - Nome: *João Vitor de Oliveira Santos*
 - Email: *jvitor2000@outlook.com*
-- Turma: *T01*
+- Turma: *IMD0030-T01*
 
 # Questões Finalizadas
 
--- negativos5/src/main.cpp
+++ negativos5/src/main.cpp
@@ -4,9 +4,36 @@ using std::cout;
 
 const int SIZE = 5; // input size.
 
+int *getValues(int length); 
+int countNegatives(int length, int* values);
+
 int main(void)
 {
-    // TODO: Adicione aqui seu código.
+    int* values = getValues(SIZE);
+
+    int counter = countNegatives(SIZE, values);
+
+    cout << counter << "\n";
 
     return 0;
+}
+
+int* getValues(int length){
+    int *values = new int[length];
+
+    for(int index = 0; index < length; index++){
+        cin >> values[index];
+    }
+
+    return values;
+}
+
+int countNegatives(int length, int* values){
+    int counter = 0;
+
+    for(int index = 0; index < length; index++){
+        if(values[index] < 0) counter++;
+    }
+
+    return counter;
 }
