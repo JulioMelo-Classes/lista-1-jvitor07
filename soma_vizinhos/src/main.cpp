eb6d03e15d8325111c51595807711eac18ab69f0
João Oliveira <jvitor2000@outlook.com>
2021-06-09 09:07:37 -0300

    implementa funcoes-falta loop

--- soma_vizinhos/src/main.cpp
+++ soma_vizinhos/src/main.cpp
@@ -8,9 +8,54 @@ using std::cout;
 using std::cin;
 using std::endl;
 
+int countPositiveValues(int m, int n);
+int countNegativeValues(int m, int n);
+int countValues(int m, int n);
+
 int main( void )
 {
-    // TODO: Adicione seu código aqui. 
+    while(true)
+    {
+        int m;
+        int n;
+
+        cin >> m;
+        cin >> n;
+
+        int value = countValues(m, n);
+
+        cout << value << "\n";
+
+        if(n == 0) break;
+    }
 
     return 0;
+}
+
+int countPositiveValues(int m, int n)
+{
+    int value = 0;
+    for(int index = m; index < m + n; index++){
+        value += index;
+    }
+
+    return value;
+}
+
+int countNegativeValues(int m, int n)
+{
+    int value = 0;
+
+    for(int index = m; index > m + n; index--){
+        value += index;
+    }
+
+    return value;
+}
+
+int countValues(int m, int n)
+{
+    if(n == 0) return m;
+
+    return n > 0 ? countPositiveValues(m, n) : countNegativeValues(m, n);
 }
